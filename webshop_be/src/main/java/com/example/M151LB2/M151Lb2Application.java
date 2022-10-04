0d9ab2c127fd99f88a5e8c4455635b80a37d5b1a
Shawn Lacarta <shawnlacarta@Shawns-MacBook-Pro.local>
2022-10-04 17:12:53 +0200

    added new be

---
+++ webshop_be/src/main/java/com/example/M151LB2/M151Lb2Application.java
@@ -0,0 +1,13 @@
+package com.example.M151LB2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M151Lb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M151Lb2Application.class, args);
+	}
+
+}
