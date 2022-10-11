af6b57e42da84dfde1472ce0a09d51c9fedbf41c
Anojan Pirabakaran <69903847+anojanpirabakaran@users.noreply.github.com>
2022-10-11 19:44:47 +0200

    add new backend

---
+++ webshop_be/src/main/java/com/example/webshop_be/WebshopBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.webshop_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopBeApplication.class, args);
+	}
+
+}
