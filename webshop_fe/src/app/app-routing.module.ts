12797b94c5bfdd9c17ab01c473c2c5cd063fa00d
Shawn Lacarta <shawnlacarta@Shawns-MacBook-Pro.local>
2022-10-17 16:55:51 +0200

    added routing and new components

--- webshop_fe/src/app/app-routing.module.ts
+++ webshop_fe/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'profile', component: ProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
