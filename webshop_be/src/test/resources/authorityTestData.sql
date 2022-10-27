705518efc30e4fd988660af5457e7ce785d8bc80
Anojan Pirabakaran <69903847+anojanpirabakaran@users.noreply.github.com>
2022-10-27 13:44:23 +0200

    add unit test

---
+++ webshop_be/src/test/resources/authorityTestData.sql
@@ -0,0 +1,6 @@
+insert into public.authority (id, name)
+values ('1', 'USER_CREATE'),
+       ('2', 'USER_MODIFY_GLOBAL'),
+       ('3', 'USER_DEACTIVATE'),
+       ('4', 'USER_SEE_GLOBAL'),
+       ('5', 'USER_SEE_OWN');
---
+++ webshop_be/src/test/resources/brandTestData.sql
@@ -0,0 +1,6 @@
+insert into public.brand (id, name)
+values ('1', 'Nike'),
+       ('2', 'Adidas'),
+       ('3', 'Jordan'),
+       ('4', 'Puma'),
+       ('5', 'Karl Kani');
